5462c3c994c1b7c8c94369f69b5d6f505ab8ad74
dduuukk <bender.c@northeastern.edu>
2023-02-19 12:23:53 -0500

    Changed Player Enumerator to JS

---
+++ model/board.js
@@ -0,0 +1,23 @@
+// import {Player} from 'C:\1. University\Spring 2023\4. SOFTWARE ENGINEERING\Reversi-Fall-2023-Team-2\model\player.js'
+
+class Board {
+    constructor(size){
+        this.size = size;
+        
+    }
+}
+
+
+
+// from model.player import Player
+
+// class Board:
+//     def __init__(self, size) -> None:
+//         self.size = size
+//         self.grid = [[0]*size for _ in range(size)]
+        
+//     def __getitem__(self, location):
+//         return self.grid[location[0]][location[1]]
+    
+//     def __setitem__(self, location, player: Player):
+//         self.grid[location[0]][location[1]] = player
---
+++ model/player.js
@@ -0,0 +1,25 @@
+export class Player {
+    static X = new Player(1);
+    static O = new Player(2);
+
+    constructor(player_marker) {
+        this.player_marker = player_marker;
+    }
+
+    enumerate() {
+        return this.player_marker;
+    }
+}
+
+console.log(Object.keys(Player));
+console.log(Player.enumerate.O);
+
+
+
+
+// Python example for reference
+// from enum import IntEnum
+
+// class Player(IntEnum):
+//     X = 1
+//     O = 2
