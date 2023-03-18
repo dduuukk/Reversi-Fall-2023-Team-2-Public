2112f3361cf39461d0ec320f75d5ce95637e032d
wisnerl <lukewisner01@gmail.com>
2023-03-18 14:28:44 -0400

    Confirmed connection to our database

--- scripts/databse_testing.cjs
+++ scripts/databse_testing.cjs
@@ -3,23 +3,24 @@ var mysql = require('mysql2');
 var db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "SQL55tuxy!!",
-  database: "test_db"
+  password: "password",
+  database: "luketest"
 });
 
 db.connect((err) => {
   if(err) {
     throw err;
   }
-  console.log('MySQL Connected...')
+  console.log('MySQL Connected!')
 });
 
-// var sql = "INSERT INTO users (id, first_name, last_name) VALUES ('00001', 'Chris', 'Bender')";
-// db.query(sql, (err, res) =>{
-//   return console.log(res)
-// });
+var sql = "INSERT INTO users (id, first_name, last_name) VALUES ('00001', 'Chris', 'Bender')";
+db.query(sql, (err, res) =>{
+  return console.log(res)
+});
 
 db.query(`SELECT * FROM test_db.users`, (err, res) =>{
   return console.log(res)
 });
+
 
