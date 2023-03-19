14b685fc007ff7d54fe4395e27ff9169c8474bdf
wisnerl <lukewisner01@gmail.com>
2023-03-18 22:15:34 -0400

    database communication and convert to string value example

--- scripts/databse_testing.cjs
+++ scripts/databse_testing.cjs
@@ -16,18 +16,25 @@ db.connect((err) => {
 
 var table = "player";
 var columns = ["username", "password", "elo", "chipcolor", "num_games_won", "num_games_lost"];
-var values = ["testplayer", "password", 500, "black", 0, 0];
+var values = ["testplayer2", "password", 500, "black", 0, 0];
 var col_str = columns.join(", ");
 var val_str = values.map(a => `\'${a}\'`).join(", ");
 
-var sql = `INSERT INTO ${table} (${col_str}) VALUES (${val_str})`;
+//var sql = `INSERT INTO ${table} (${col_str}) VALUES (${val_str})`;
+var sql = `SELECT ${"username"} FROM ${table}`;
 console.log(sql);
 
 db.query(sql, (err, res) =>{
   console.log("queried");
-  return console.log(res);
+  console.log(res);
+  var testArray = Object.values(res[0]);
+  var firstPlayer = String(testArray);
+  console.log(firstPlayer);
 });
 
+// db.end(sql, (err, res) => {
+
+// });
 
 // var column_array = ['username']
 // var col_str = column_array.join(", ");
