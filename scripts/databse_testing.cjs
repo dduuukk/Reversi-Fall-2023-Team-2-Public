1a5a5f490c1ef1a37fbd13f80272c511ca4fa349
dduuukk <bender.c@northeastern.edu>
2023-03-18 21:25:46 -0400

    More database testing changes

--- scripts/databse_testing.cjs
+++ scripts/databse_testing.cjs
@@ -15,33 +15,43 @@ db.connect((err) => {
 });
 
 var table = "player";
-var column_array = ['idplayer']
-var col_str = column_array.join(", ");
+var columns = ["username", "password", "elo", "chipcolor", "num_games_won", "num_games_lost"];
+var values = ["testplayer", "password", 500, "black", 0, 0];
+var col_str = columns.join(", ");
+var val_str = values.map(a => `\'${a}\'`).join(", ");
 
-var sql = `SELECT ${col_str} FROM ${table}`;
-console.log(sql)
+var sql = `INSERT INTO ${table} (${col_str}) VALUES (${val_str})`;
+console.log(sql);
+
+db.query(sql, (err, res) =>{
+  console.log("queried");
+  return console.log(res);
+});
 
 
+// var column_array = ['username']
+// var col_str = column_array.join(", ");
 
-
-// var columns = ["idplayer", "username", "password", "elo", "chipcolor", "num_games_won", "num_games_lost"];
-// var values = [00004, "testplayer4", "password", 500, "black", 0, 0];
-// var col_str = columns.join(", ");
-// var val_str = values.map(a => `\'${a}\'`).join(", ");
-
-// var sql = `INSERT INTO ${table} (${col_str}) VALUES (${val_str})`;
+// var sql = `SELECT ${col_str} FROM ${table}`;
 // console.log(sql);
+
+
+
+
+
 
 // var sql = `INSERT INTO player (idplayer, username, password, elo, chipcolor, num_games_won, num_games_lost) VALUES ('4', 'testplayer4', 'password', '500', 'black', '0', '0')
 // var sql = `INSERT INTO player (idplayer, username, password, elo, chipcolor, num_games_won, num_games_lost) VALUES ('00003', 'testplayer3', 'password', '500', 'black', '0', '0')`;
-function get_info(sql, callback) {
-  db.query(sql, (err, res) =>{
-    console.log("queried");
-    console.log(res);
-    return callback(res);
-  })
-}
+// function get_info(sql, callback) {
+//   db.query(sql, (err, res) =>{
+//     console.log("queried");
+//     console.log(res);
+//     return callback(res);
+//   })
+// }
 
+// var stuff = ``;
+// get_info
 
 
 // console.log("temp:", temp);
